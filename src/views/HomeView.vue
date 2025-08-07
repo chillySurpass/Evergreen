a64875f392b0d541c8440fe93842bca2b16e4cef
linjun <1073617223@qq.com>
2025-08-07 15:18:42 +0800

    fix：bilibili-banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,36 @@
   <div class="common-layout">
     <el-container>
       <el-aside width="200px">Aside</el-aside>
-      <el-container>
-        <el-header>1</el-header>
-        <el-main>Main</el-main>
-        <el-footer>Footer</el-footer>
-      </el-container>
+      <el-header>
+        <div class="container">
+          <div class="card">
+            <BilibiliAutumn1BannerComponent></BilibiliAutumn1BannerComponent>
+          </div>
+        </div>
+      </el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
     </el-container>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "HomeView",
-  components: {},
-});
+<script setup lang="ts">
+import BilibiliAutumn1BannerComponent from "@/views/banner/inst/bilibili-autumn-1-banner.component.vue";
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  > .card {
+    width: 800px;
+    border-radius: 8px;
+    box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, 0.25);
+    overflow: hidden;
+  }
+}
+</style>
